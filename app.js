ff560a5377122b5a19e89c72bcecbbee36dab016
johnEDCO <jhedwar17@gmail.com>
2022-07-04 11:52:44 -0500

    first commit api

---
+++ app.js
@@ -0,0 +1,12 @@
+require("dotenv").config() //para que funcione bien nuestro .env en todo el entorno y poder usaar las variables de entorno
+const express = require("express")
+const cors = require("cors")
+const appp = express()
+
+appp.use(cors())
+
+const port = process.env.PORT || 3000
+
+appp.listen(port,()=>{
+    console.log(`Tu app esta lista por http://localhost:${port}`)
+})
